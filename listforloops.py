2a16b53cfcfb3e7a3f13303da2b9781c04798607
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-10 20:09:59 +0800

    update code

--- listforloops.py
+++ listforloops.py
@@ -217,6 +217,36 @@ print find_element(['alpha','beta'],'gamma')
 
 
 
+# Define a procedure, find_element,
+# using index that takes as its
+# inputs a list and a value of any
+# type, and returns the index of
+# the first element in the input
+# list that matches the value.
+
+# If there is no matching element,
+# return -1.
+
+def findindex_element(list, element):
+    if element in list:
+        return list.index(element)
+    else:
+        return -1
+
+
+
+
+
+print findindex_element([1,2,3],3)
+#>>> 2
+
+print find_element(['alpha','beta'],'gamma')
+#>>> -1
+
+
+
+
+
 
 
 
