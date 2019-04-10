13eceddc1563ce38eba1344be8b50c23e1ea4aaa
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-10 19:36:17 +0800

    measure list

--- listforloops.py
+++ listforloops.py
@@ -162,6 +162,31 @@ print sum_list([44, 14, 76])
 
 
 
+# Define a procedure, measure_udacity,
+# that takes as its input a list of strings,
+# and returns a number that is a count
+# of the number of elements in the input
+# list that start with the uppercase 
+# letter 'U'.
+
+def measure_udacity(list):
+    found = 0
+    for e in list:
+        if e[0] == 'U':
+            found += 1
+    return found
+
+
+
+print measure_udacity(['Dave','Sebastian','Katy'])
+#>>> 0
+
+print measure_udacity(['Umika','Umberto'])
+#>>> 2
+
+
+
+
 
 
 
