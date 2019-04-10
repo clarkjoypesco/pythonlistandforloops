1c88279fca6d7d23affd0996241213f3ff9ed436
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-10 18:43:27 +0800

    replace spy

--- listforloops.py
+++ listforloops.py
@@ -15,3 +15,46 @@ stooges = ['Moe','Larry','Curly']
 
 stooges[2] = 'Shemp'
 print stooges
+
+#testmode
+
+p = ['H', 'e', 'l', 'l', 'o']
+q = p
+print p
+print q
+q[0] = 'Y'
+print p
+print q
+
+#secret agent man
+# What is the value of agent[2] after the following code runs?
+spy = [0,0,7]
+agent = spy
+spy[2] = agent[2] + 1
+
+#spy should print 8
+
+print agent[2]
+
+
+
+# Define a procedure, replace_spy,
+# that takes as its input a list of
+# three numbers, and modifies the
+# value of the third element in the
+# input list to be one more than its
+# previous value.
+
+
+def replace_spy(list = [3]):
+    list[2] = list[2] + 1
+    
+spy = [0,0,7]
+
+# In the test below, the first line calls your 
+# procedure which will change spy, and the 
+# second checks you have changed it.
+# Uncomment the top two lines below.
+replace_spy(spy)
+print spy
+#>>> [0,0,8]
